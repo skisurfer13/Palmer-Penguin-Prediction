ce31537738841b587cd05384aad1798fad08cdd6
Pranav Sawant <skisurfer13@gmail.com>
2024-01-26 03:39:40 -0600

    App.py

--- App.py
+++ App.py
@@ -1,5 +1,3 @@
-#Certainly! Here's the corrected code:
-
 
 #import statements
 import streamlit as st
@@ -14,7 +12,6 @@ import os
 st.write("""
 # Penguin Prediction App
 - This app predicts the species of Palmer penguins found in Antarctica using Machine Learning!
-- App built by Pranav Sawant and Anshuman Shukla of Team Skillocity.
 - Dataset credits: Dr. Kristen Gorman and Palmer Station, Antarctica LTER and Allison Horst.
 - Note: User inputs for features are taken from the sidebar. It is located at the top left of the page (arrow symbol). The values of parameters can be changed from the sidebar.
 """)
@@ -84,13 +81,7 @@ elif penguins_species[prediction] == 'Adelie':
     st.image(load_image('adelie.jpg'))
 
 # Important info
-st.sidebar.subheader("An article about this app: https://proskillocity.blogspot.com/2021/05/penguin-classification-and-prediction.html")
 st.write("Dataset citation: Gorman KB, Williams TD, Fraser WR (2014). Ecological sexual dimorphism and environmental variability within a community of Antarctic penguins (genus Pygoscelis). PLoS ONE 9(3):e90081. https://doi.org/10.1371/journal.pone.0090081")
 st.write("Dataset License: Creative Commons 0")
 st.write("About the dataset: This dataset was created by Dr. Kristen Gorman and members of the Palmer Station, Antarctica (LTER). Palmer is one of the three US Antarctic Stations governed by the Antarctic Treaty of 1959. The Palmer Station is an interdisciplinary polar marine research program established in 1990.")
 st.write("The dataset was uploaded by Allison Horst and is available under the CC-0 license in accordance with the Palmer Station LTER Data Policy and the LTER Data Access Policy for Type 1 data. The dataset contains data for 344 penguins, including three different species collected from three islands in the Palmer Archipelago, Antarctica.")
-
-# Skillocity
-image = Image.open('killocity (3).png')
-st.image(image, use_column_width=True)
-
